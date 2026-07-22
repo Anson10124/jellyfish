f325b01ee1a1e9f5e4b1dbceef20984aabbc7c30
Anson10124 <115175517+Anson10124@users.noreply.github.com>
2026-07-22 22:05:12 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider"
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Jellyfish",
+  description: "A web third party client for jellyfin with integration with Seerr",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >{children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
